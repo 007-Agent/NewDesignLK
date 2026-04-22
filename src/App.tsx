2b9f5412d0d64a5e293444233077d8ac3430f76a
Абаничев Даниил <abanichev@kdpmc.ru>
2026-04-22 15:23:39 +0300

    22/04

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from './redux/hooks';
 import { Header } from './components/Header/Header';
-import { ProfilePage } from './RouterSlide/Profile/ProfilePage.tsx';
+import { ProfilePage } from './RouterSlide/Profile/ProfilePage';
 import { PatientsPage } from './components/PatientsPage';
-import { SchedulePage } from './RouterSlide/Shedule/SchedulePage.tsx';
-import { AppointmentsPage } from '../src/RouterSlide/Appointments/AppointmentsPage.tsx'
+
+import { SchedulePage } from './RouterSlide/Shedule/SchedulePage';
+import { AppointmentsPage } from '../src/RouterSlide/Appointments/AppointmentsPage'
 import { Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { PatientDetailPageWrapper } from './components/patientDetailsFull/PatientWrapperDetail/PatientDetailWrapper';
-import Policy from './RouterSlide/Policy/Policy.tsx';
+import Policy from './RouterSlide/Policy/Policy';
 import Layout from '../src/components/Layout';
 import './App.scss'
-import { fetchSpecialties } from './redux/Departament/Specialities.ts';
+import { fetchSpecialties } from './redux/Departament/Specialities';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
--- src/components/Login/Auth/Authorization.tsx
+++ src/components/Login/Auth/Authorization.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useAppDispatch } from '../../../redux/hooks';
 import { loginUser } from '../../../redux/authSlice';
+import { useNavigate } from 'react-router-dom';
 import "../login.scss"
 interface AuthorizationProps {
   onSuccess?: () => void; // закрыть модалку после успешного входа
@@ -12,7 +13,7 @@ export function Authorization({ onSuccess }: AuthorizationProps) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const dispatch = useAppDispatch();
-
+  const navigate = useNavigate();
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setError(null);
@@ -25,6 +26,7 @@ export function Authorization({ onSuccess }: AuthorizationProps) {
       setError(err.message || 'Ошибка авторизации');
     } finally {
       setLoading(false);
+      navigate('/doctors');
     }
   };
 
