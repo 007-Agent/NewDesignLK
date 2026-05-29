304eeaff84d390640917eb52654cec2adf952672
Абаничев Даниил <abanichev@kdpmc.ru>
2026-05-29 13:18:27 +0300

    Новые изменения по ЛК-29-1

--- src/components/Header/Icon/ProfileLogoutIcon .tsx
+++ src/components/Header/Icon/ProfileLogoutIcon .tsx
@@ -15,19 +15,29 @@ const dispatch = useAppDispatch();
     
  
   };
+ const handleGoToProfile = () => {
+  // ← останавливаем всплытие!
+  console.log('Клик по профилю, переход на /profile');
+  navigate('/profile');
+};
   return (
    <div className="absolute flex flex-col items-center p-0 bg-white rounded-lg shadow-lg top-full right-10 mt-2 z-50 min-w-[120px]">
-      <div className="flex items-center gap-2 px-4 py-3 w-full cursor-pointer transition-colors duration-200 hover:bg-gray-100 border-b border-gray-200">
-        <User size={27} strokeWidth={1.5} />
-        <span className="text-sm text-gray-700">профиль</span>
-      </div>
-      <div 
-        className="flex items-center gap-2 px-4 py-3 w-full cursor-pointer transition-colors duration-200 hover:bg-gray-100"
-        onClick={handleExitUser}
-      >
-        <LogOut size={27} strokeWidth={1.5} />
-        <span className="text-sm text-gray-700">Выход</span>
-      </div>
-    </div>
+  <button
+    type="button"
+    className="flex items-center gap-2 px-4 py-3 w-full cursor-pointer transition-colors duration-200 hover:bg-gray-100 border-b border-gray-200 bg-transparent"
+    onClick={handleGoToProfile}
+  >
+    <User size={27} strokeWidth={1.5} />
+    <span className="text-sm text-gray-700">профиль</span>
+  </button>
+  <button
+    type="button"
+    className="flex items-center gap-2 px-4 py-3 w-full cursor-pointer transition-colors duration-200 hover:bg-gray-100 bg-transparent"
+    onClick={handleExitUser}
+  >
+    <LogOut size={27} strokeWidth={1.5} />
+    <span className="text-sm text-gray-700">Выход</span>
+  </button>
+</div>
   );
 };
